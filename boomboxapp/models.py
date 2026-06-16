8e247db3e7a929349cbc90b7c86997d8d14c0726
bbbertucci <bertucci.b.bruno@gmail.com>
2026-06-15 22:47:04 -0300

    adding first exercises

--- boomboxapp/models.py
+++ boomboxapp/models.py
@@ -10,3 +10,18 @@ class Account(models.Model):
         constraints = [
             models.UniqueConstraint(fields=['docnumber'], name='account_docnumber_unique')
         ]
+
+class Album(models.Model):
+    album_name = models.CharField()
+    creation_date = models.DateField()
+    release_date = models.DateField()
+
+class Rating(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    creation_date = models.DateField()
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['account', 'album'], name='rating_account_album_unique')
+        ]
--- boomboxapp/views.py
+++ boomboxapp/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
+import json
+from . import models
+from datetime import datetime
 
 # Create your views here.
 
 from django.http import HttpResponse
 
 def create_account(request):
-    return HttpResponse ('Account Created')
+    body = request.body
+    dct = json.loads(body)
+    
+    """ TODO dct is a dictionary with keys:
+    username, docnumber, password, and birthdate (format 'DD/MM/YYYY')
+    1) Assign the birthdate of the dictionary to a separate variable
+    2) Split the birthdate by its separator '/'
+    3) Assign each component of the split birthdate into new variables,
+        casting the day and month (but not year) to integers
+    4) For the year component, use list slicing to separate the first
+        two digits of the year and the last two into separate variables,
+        also casting them to integers
+    5) Sum the day + month + first_half_year + second_half_year
+    6) Get the modulo of this sum by 16 and return it in the response,
+        formatting it into a user-friendly message 
+    """
+
+    return HttpResponse ('')
+
+def save_ratings(request):
+    return HttpResponse ('Ratings Saved')
